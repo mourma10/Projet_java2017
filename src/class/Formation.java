265064bfd35ed6abc142562ac3018586317b4d57
amam37 <elhadjiamadou.gueye@gmail.com>
2017-03-21 16:55:25 +0000

    Add files via upload

---
+++ src/class/Formation.java
@@ -0,0 +1,21 @@
+public class Formation {
+	
+	private int idFormation ;
+	private String departement ;
+	private String niveau ;
+	private String option ;
+	private String annee ;
+
+	public Formation (int idFormation , String departement , String niveau ,
+					String option , String annee ){
+
+
+		this.idFormation=idFormation ;
+		this.departement=departement;
+		this.niveau=niveau;
+		this.option=option ;
+		this.annee=annee;
+	}
+
+
+}
